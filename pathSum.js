6804c3789a8690888ff29a7740c0151e1f3b933b
Cali Tabilas <ctabilas23@gmail.com>
2019-08-27 10:08:05 -0400

    optimized pathsum

--- pathSum.js
+++ pathSum.js
@@ -1,14 +1,23 @@
+// Initial Solution
+// const hasPathSum = (root, sum) => {
+//   const traverse = (node, current) => {
+//     if (!node) {
+//       return false;
+//     }
+//     if (!node.left && !node.right) {
+//       return sum === current + node.val;
+//     }
+//     let leftPath = traverse(node.left, current + node.val);
+//     let rightPath = traverse(node.right, current + node.val);
+//     return leftPath || rightPath;
+//   };
+//   return traverse(root, 0);
+// };
+
+// Another solution, faster and less memory
 const hasPathSum = (root, sum) => {
-  const traverse = (node, current) => {
-    if (!node) {
-      return false;
-    }
-    if (!node.left && !node.right) {
-      return sum === current + node.val;
-    }
-    let leftPath = traverse(node.left, current + node.val);
-    let rightPath = traverse(node.right, current + node.val);
-    return leftPath || rightPath;
-  };
-  return traverse(root, 0);
+  if (!root) return false;
+  sum -= root.val;
+  if (!root.right && !root.left) return !sum;
+  return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
 };
