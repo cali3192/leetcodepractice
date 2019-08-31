c6d52f81407f3c5b02788748346dafd5339ebe4c
Cali Tabilas <ctabilas23@gmail.com>
2019-08-30 22:56:23 -0400

    typescrpt play

--- helloTypescript.js
+++ helloTypescript.js
@@ -1,7 +1,29 @@
 // console.log(`Hello Typescript`);
+// stricter you are with types, it prevents you from making compiler erroers
 var sayHello = function (str) {
     console.log("hello " + str);
     return "hello " + str;
 };
-// sayHello('John');
-sayHello(1);
+sayHello('Typescript');
+// sayHello(1);
+// boolean example
+var opposite = function (bool) {
+    return !bool;
+};
+var addExcitement = function (arr) {
+    return arr.map(function (name) {
+        return name + "!!!";
+        // return 1;
+    });
+};
+var names = ['Andrew', 'Brian', 'Soumith'];
+// error
+// const names = ['Andrew', 'Brian', 'Soumith', 1];
+var excitedNamess = addExcitement(names);
+console.log('excitedNamess', excitedNamess);
+// void functions, no return :(
+var starkMotto = function () {
+    console.log("Winter is coming");
+    // return `hi`;
+};
+starkMotto();
--- helloTypescript.ts
+++ helloTypescript.ts
@@ -1,4 +1,5 @@
 // console.log(`Hello Typescript`);
+// stricter you are with types, it prevents you from making compiler erroers
 
 const sayHello = (str: String) => {
   console.log(`hello ${str}`);
@@ -7,3 +8,29 @@ const sayHello = (str: String) => {
 
 sayHello('Typescript');
 // sayHello(1);
+
+// boolean example
+const opposite = (bool: Boolean) => {
+  return !bool;
+};
+
+const addExcitement = (arr: String[]): String[] => {
+  return arr.map((name) => {
+    return `${name}!!!`;
+    // return 1;
+  });
+};
+
+const names = ['Andrew', 'Brian', 'Soumith'];
+// error
+// const names = ['Andrew', 'Brian', 'Soumith', 1];
+const excitedNamess = addExcitement(names);
+console.log('excitedNamess', excitedNamess);
+
+// void functions, no return :(
+const starkMotto = (): void => {
+  console.log(`Winter is coming`);
+  // return `hi`;
+};
+
+starkMotto();
