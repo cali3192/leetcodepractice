59613e1baf36f4d66ec34773df3c84d096be7239
Charmaine Tabilas <charmainetabilas@Charmaines-MacBook-Air-3.local>
2019-09-04 08:15:27 -0400

    working largestAdjSolution

---
+++ codesignalSandbox.js
@@ -0,0 +1,17 @@
+// largestadjecent product
+const largestAdjecentProductArr = [1, 2, -2, -3, 7, 3];
+
+const largestAdjecent = (arr) => {
+  let result = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    const elem = arr[i];
+    const pre = arr[i - 1];
+    const prod = elem * pre;
+    if (prod > result) result = prod;
+  }
+
+  return result;
+};
+
+console.log('largestAdjecent() \n', largestAdjecent(largestAdjecentProductArr));
