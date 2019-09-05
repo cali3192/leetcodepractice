90832719baa13f2ad8f02bdd5b75faf81121583f
Charmaine Tabilas <charmainetabilas@Charmaines-MacBook-Air-3.local>
2019-09-04 21:43:05 -0400

    finished isLucky

--- codesignalSandbox.js
+++ codesignalSandbox.js
@@ -14,10 +14,10 @@ const largestAdjecent = (arr) => {
   return result;
 };
 
-console.log(
-  '1. largestAdjecent() \n',
-  largestAdjecent(largestAdjecentProductArr)
-);
+// console.log(
+//   '1. largestAdjecent() \n',
+//   largestAdjecent(largestAdjecentProductArr)
+// );
 
 function almostIncreasingSequence(sequence) {
   let result = 0;
@@ -55,4 +55,122 @@ function matrixElementsSum(matrix) {
 
 const matrixTest = [[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]];
 
-console.log('matrixElementsSum(matrixTest)', matrixElementsSum(matrixTest));
+// console.log('matrixElementsSum(matrixTest)', matrixElementsSum(matrixTest));
+
+function allLongestStrings(inputArray) {
+  const longest = inputArray.sort((a, b) => {
+    return b.length - a.length;
+  })[0].length;
+
+  return inputArray.filter((word) => {
+    return word.length === longest;
+  });
+}
+
+inputArray = ['aba', 'aa', 'ad', 'vcd', 'aba'];
+const allLongestActual = allLongestStrings(inputArray);
+// console.log('allLongestActual', allLongestActual);
+
+const makeObj = (str) => {
+  const obj = {};
+  str.split('').forEach((letter) => {
+    if (!obj.hasOwnProperty(letter)) {
+      obj[letter] = 1;
+    } else {
+      obj[letter]++;
+    }
+  });
+  return obj;
+};
+
+function commonCharacterCount(s1, s2) {
+  let obj1 = makeObj(s1);
+  let obj2 = makeObj(s2);
+
+  let total = 0;
+
+  for (const key in obj1) {
+    if (obj2.hasOwnProperty(key)) {
+      if (obj2[key] < obj1[key]) {
+        total += obj2[key];
+      } else {
+        total += obj1[key];
+      }
+    }
+  }
+
+  return total;
+}
+
+let s1 = 'aabcc';
+let s2 = 'adcaa';
+
+const commonCharacterCountActual = commonCharacterCount(s1, s2);
+// console.log('commonCharacterCountActual', commonCharacterCountActual);
+
+const removeFirstLastZero = (str) => {
+  if (str[0] === '0') {
+    return str.slice(1, str.length);
+  }
+
+  if (str[str.length - 1] === '0') {
+    return str.slice(0, str.length - 1);
+  }
+
+  return str;
+};
+
+// function isLucky(n) {
+//   const numStr = `${n}`;
+//   const half = numStr.length / 2;
+//   // return numStr.substring(0, half);
+
+//   const obj = {};
+//   numStr.split('').forEach((elem) => {
+//     if (!obj.hasOwnProperty(elem)) {
+//       obj[elem] = true;
+//     }
+//   });
+
+//   if (Object.keys(obj).length === 1) {
+//     return true;
+//   }
+
+//   if (
+//     numStr
+//       .substring(0, half)
+//       .split('')
+//       .reverse()
+//       .join('') === numStr.substring(half, numStr.length)
+//   ) {
+//     return true;
+//   }
+//   let firstSum = numStr
+//     .substring(0, half)
+//     .split('')
+//     .reduce((a, b) => {
+//       return parseFloat(a) + parseFloat(b);
+//     });
+
+//   let second = numStr.substring(half, numStr.length);
+//   let secondTrimmed = parseFloat(removeFirstLastZero(second));
+//   return firstSum === secondTrimmed;
+// }
+
+const sumString = (str) => {
+  return str.split('').reduce((a, b) => {
+    return parseFloat(a) + parseFloat(b);
+  });
+};
+
+const isLucky = (n) => {
+  const toStr = `${n}`;
+  const half = toStr.length / 2;    
+  return (
+    sumString(toStr.substr(half, toStr.length)) ===
+    sumString(toStr.substr(0, half))
+  );
+};
+
+const isLuckyActual = isLucky(1203);
+console.log('isLuckyActual', isLuckyActual);
