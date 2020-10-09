fd9d1fd90280266995c7641fd7e5d09bb8d3841d
Cali Tabilas <ctabilas23@gmail.com>
2020-10-08 21:28:03 -0400

    completed linked list palindrome

---
+++ Leetcode/easy/linkedLists/mergeTwoSortedLinkedLists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+const mergeTwoLists = (l1, l2) => {
+  // create dummy node
+  let dummy = new ListNode(-1);
+
+  const head = dummy;
+
+  // while either exists
+  while (!!l1 || !!l2) {
+    if (l1 && (!l2 || l1.val <= l2.val)) {
+      dummy.next = l1;
+      l1 = l1.next;
+    } else {
+      dummy.next = l2;
+      l2 = l2.next;
+    }
+    // move the dummy forward
+    dummy = dummy.next;
+  }
+  return head.next;
+};
---
+++ Leetcode/easy/linkedLists/palindromeLinkedList.js
@@ -0,0 +1,71 @@
+/**
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+// helper to reverse
+const reverseList = (head) => {
+  // current node, ref to previous and a temp
+  let node = head;
+
+  // prev needs to be null because it will be the node.next
+  let prev = null;
+  let temp = null;
+
+  while (node) {
+    // save ref
+    temp = node.next;
+
+    // reverse pointer
+    node.next = prev;
+
+    // at this point, we updated temp - now update prev and node
+    prev = node;
+    node = temp;
+  }
+
+  // prev is now the head of the LL
+  return prev;
+};
+
+const isPalindrome = (head) => {
+  // find the midpoint using fast and slow pointers
+  const fast = head;
+  const slow = head;
+
+  // fast moving 2x as fast as slow
+  while (!!fast && !!fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  // move fast to head
+  fast = head;
+
+  // reverse for a new linked list
+  const reversedHalf = reverseList(slow);
+  console.log("LOG: ", { reversedHalf });
+
+  // // slow is at head of half
+  // slow = reversedHalf;
+  // console.log(`LOG: `, { slow });
+
+  // while (fast && slow) {
+  //   if (fast.val !== slow.val) {
+  //     return false;
+  //   }
+  //   fast = fast.next;
+  //   slow = slow.next;
+  // }
+
+  // // while both exist, compare nodes
+  // return true;
+};
