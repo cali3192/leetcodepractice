9dc99cf28e4db3378c0a6d1db10fe498d9d4680b
Cali Tabilas <ctabilas23@gmail.com>
2020-12-16 23:37:14 -0500

    completed balanced brackets

--- algoExpert/medium/balancedBrackets.js
+++ algoExpert/medium/balancedBrackets.js
@@ -1,32 +1,44 @@
 const balancedBrackets = (string) => {
+  // defining stack and putting first elmeent in strnig in
   const stack = [string[0]];
 
+  // defining pairs
   const brackets = {
     "[": "]",
     "{": "}",
     "(": ")",
   };
 
+  // string to find closing brackets
   const closing = "]})";
 
+  // loopting through elements
   for (let i = 1; i < string.length; i++) {
+    // current bracket
     const bracket = string[i];
 
+    // if opening brackets, put in stack (opening brackets are keys)
     if (brackets.hasOwnProperty(bracket)) {
+      // push only opening brackets
       stack.push(bracket);
     }
 
+    // if we're dealing with closing brackets
     if (closing.includes(bracket)) {
-      console.log({ stack, bracket });
+      // get last elem in stack
       const lastBracket = stack[stack.length - 1];
+
+      // the closing bracket should match the last bracket in the stack
       if (brackets[lastBracket] !== bracket) {
         return false;
       } else {
+        // pop the last opening bracket and move on
         stack.pop();
       }
     }
   }
 
+  // our stack should be empty at the end
   return stack.length === 0;
 };
 
