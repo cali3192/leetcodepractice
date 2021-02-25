2d07d5bcf57d17502b106e3a6c5c77d779ff8486
Cali Tabilas <ctabilas23@gmail.com>
2021-02-25 14:35:40 -0500

    moved folders

--- amazon/maxIslandArea/index.js
+++ amazon/maxIslandArea/index.js
@@ -1,53 +1,50 @@
-/*
- * Complete the 'combineParts' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY parts as parameter.
- *
- * 1. combine 2 at a time
- * 2. parts together is equal to partsSizes
- *
+/**
+ * @param {number[][]} grid
+ * @return {number}
  */
-
-function combineParts(parts) {
-  console.log(`input`, { parts });
-
-  // Write your code here
-  if (parts.length === 1) return 0;
-
-  // nlogn
-  // sorting our input
-  parts.sort((a, b) => a - b);
-
-  // starting at second elem
-  let i = 1;
-
-  // result array to track sums
-  let res = [];
-
-  // while there are more than 1 part
-  while (parts.length >= 2) {
-    // get the sum of current elem and previous elem
-    let sum = parts[i] + parts[i - 1];
-
-    console.log({ sum });
-    // set the new sum to parts[1]
-    parts[i] = sum;
-
-    // add the current sum to our result array
-    res.push(sum);
-
-    // remove the first element in parts
-    parts = parts.slice(1);
-
-    // sort so that we are adding the 2 smallest values
-    parts = parts.sort((a, b) => a - b);
-
-    console.log({ parts, res });
+var maxAreaOfIsland = function (grid) {
+  let maxArea = 0;
+  const visited = Array.from({ length: grid.length }, () =>
+    new Array(grid[0].length).fill(false)
+  );
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === 0) continue;
+      if (visited[row][col]) continue;
+      maxArea = Math.max(dfs(row, col, grid, visited), maxArea);
+    }
   }
+  return maxArea;
+};
 
-  // our final result is the sum of all the sums
-  let result = res.reduce((a, b) => a + b);
-  console.log({ result });
-  return result;
-}
+const dfs = (row, col, grid, visited) => {
+  if (visited[row][col]) return 0;
+  visited[row][col] = true;
+  let maxArea = 0;
+  const neighbors = getNeighbors(row, col, grid, visited);
+  for (let neighbor of neighbors) {
+    maxArea += dfs(neighbor[0], neighbor[1], grid, visited);
+  }
+  return grid[row][col] + maxArea;
+};
+
+const getNeighbors = (row, col, grid, visited) => {
+  const rowC = [0, 0, 1, -1];
+  const colC = [1, -1, 0, 0];
+  let neighbors = [];
+  for (let i = 0; i < 4; i++) {
+    let currentRow = row + rowC[i],
+      currentCol = col + colC[i];
+    if (currentRow >= grid.length || currentCol >= grid[0].length) continue;
+    if (currentRow < 0 || currentCol < 0) continue;
+    if (visited[currentRow][currentCol]) continue;
+    if (grid[currentRow][currentCol] === 0) continue;
+    neighbors.push([currentRow, currentCol]);
+  }
+  return neighbors;
+};
+
+
+
+
+
